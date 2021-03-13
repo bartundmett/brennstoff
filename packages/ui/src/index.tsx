3014725d2feef705fdccb93d7d7507215ddca160
Benedikt Grimm <benedikt.grimm@twt.de>
2021-03-13 17:51:13 +0100

    CHANGED dependencies to peer dependencies

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,3 @@
-export * from '@brennstoff/core';
-export * from '@brennstoff/basetheme';
-export * from '@brennstoff/button';
+export { Brennstoff, styled } from '@brennstoff/core';
+export { default as theme } from '@brennstoff/basetheme';
+export { default as Button } from '@brennstoff/button';
